b2335c7c833a85e8b9e96034f5ef5ffb25def44d
dudad <damian.dudzinski@gmail.com>
2021-12-30 20:35:05 +0100

    Bookseller: 2nd passing test

--- Go/bookseller/main.go
+++ Go/bookseller/main.go
@@ -1,5 +1,17 @@
 package bookseller
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func StockList(listArt []string, listCat []string) string {
-	return "(A - 20)"
+	sum := 0
+	for _, book := range listArt {
+		valStr := strings.Fields(book)[1]
+		val, _ := strconv.Atoi(valStr)
+		sum += val
+	}
+	return fmt.Sprintf("(A - %d)", sum)
 }
--- Go/bookseller/main_test.go
+++ Go/bookseller/main_test.go
@@ -3,8 +3,15 @@ package bookseller
 import "testing"
 
 func TestSingleBook(t *testing.T) {
-	grouped := StockList([]string{"ABC 10"}, []string{"A"})
+	grouped := StockList([]string{"ABC 20"}, []string{"A"})
 	if grouped != "(A - 20)" {
-		t.Error()
+		t.Error("Retrurned:", grouped)
+	}
+}
+
+func TestTwoBooksInTheSameCategory(t *testing.T) {
+	grouped := StockList([]string{"ABC 10", "AUY 20"}, []string{"A"})
+	if grouped != "(A - 30)" {
+		t.Error("Retrurned:", grouped)
 	}
 }
