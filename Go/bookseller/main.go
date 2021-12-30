896b6c47be107e7dbd5d5e04e7cc40a486f769df
dudad <damian.dudzinski@gmail.com>
2021-12-30 21:23:58 +0100

    Bookseller: added egde case when arrays are empty

--- Go/bookseller/main.go
+++ Go/bookseller/main.go
@@ -1,17 +1,41 @@
 package bookseller
 
 import (
+	"bytes"
 	"fmt"
 	"strconv"
 	"strings"
 )
 
-func StockList(listArt []string, listCat []string) string {
-	sum := 0
-	for _, book := range listArt {
-		valStr := strings.Fields(book)[1]
-		val, _ := strconv.Atoi(valStr)
-		sum += val
+func categoryMapToString(categoryMap map[string]int, listCat []string) string {
+	if len(categoryMap) == 0 || len(listCat) == 0 {
+		return ""
 	}
-	return fmt.Sprintf("(A - %d)", sum)
+	var buffer bytes.Buffer
+	for _, cat := range listCat {
+		buffer.WriteString(fmt.Sprintf(" - (%s : %d)", cat, categoryMap[cat]))
+	}
+	return buffer.String()[3:]
+}
+
+func pareBookString(book string) (string, int) {
+	items := strings.Fields(book)
+	name := items[0]
+	valStr := items[1]
+	val, _ := strconv.Atoi(valStr)
+
+	return name[0:1], val
+}
+
+func buildCategoryMap(listArt []string) map[string]int {
+	categoryMap := make(map[string]int)
+	for _, book := range listArt {
+		category, val := pareBookString(book)
+		categoryMap[category] += val
+	}
+	return categoryMap
+}
+
+func StockList(listArt []string, listCat []string) string {
+	return categoryMapToString(buildCategoryMap(listArt), listCat)
 }
--- Go/bookseller/main_test.go
+++ Go/bookseller/main_test.go
@@ -4,14 +4,53 @@ import "testing"
 
 func TestSingleBook(t *testing.T) {
 	grouped := StockList([]string{"ABC 20"}, []string{"A"})
-	if grouped != "(A - 20)" {
+	if grouped != "(A : 20)" {
 		t.Error("Retrurned:", grouped)
 	}
 }
 
 func TestTwoBooksInTheSameCategory(t *testing.T) {
 	grouped := StockList([]string{"ABC 10", "AUY 20"}, []string{"A"})
-	if grouped != "(A - 30)" {
+	if grouped != "(A : 30)" {
 		t.Error("Retrurned:", grouped)
 	}
 }
+
+func TestTwoBooksInDiffrentCategories(t *testing.T) {
+	grouped := StockList([]string{"ABC 10", "BUY 20"}, []string{"A", "B"})
+	if grouped != "(A : 10) - (B : 20)" {
+		t.Error("Retrurned:", grouped)
+	}
+}
+
+func TestOnlySpecifiedCategoriesAreListed(t *testing.T) {
+	grouped := StockList([]string{"ABC 10", "BUY 20", "CAT 33"}, []string{"B", "A"})
+	if grouped != "(B : 20) - (A : 10)" {
+		t.Error("Retrurned:", grouped)
+	}
+}
+
+func TestEmptyCategory(t *testing.T) {
+	grouped := StockList([]string{"ABC 10", "BUY 20", "CAT 33"}, []string{"B", "A", "E"})
+	if grouped != "(B : 20) - (A : 10) - (E : 0)" {
+		t.Error("Retrurned:", grouped)
+	}
+}
+
+
+func TestEmptyBookList(t *testing.T) {
+	grouped := StockList([]string{}, []string{"B", "A", "E"})
+	if grouped != "" {
+		t.Error("Retrurned:", grouped)
+	}
+}
+
+func TestEmptyCatList(t *testing.T) {
+	grouped := StockList([]string{"ABC 10", "BUY 20", "CAT 33"}, []string{})
+	if grouped != "" {
+		t.Error("Retrurned:", grouped)
+	}
+}
+
+
+
