3510d07369109c0c96705bcf364eddc9e802f76e
dudad <damian.dudzinski@gmail.com>
2022-01-02 19:17:40 +0100

    anagrams: 2nd test

--- Go/anagram/anagram.go
+++ Go/anagram/anagram.go
@@ -1,5 +1,8 @@
 package anagram
 
 func Anagrams(word string, words []string) []string {
-	return []string{}
+	if len(word) == 0 || len(words) == 0 {
+		return nil
+	}
+	return words
 }
--- Go/anagram/anagram_test.go
+++ Go/anagram/anagram_test.go
@@ -10,8 +10,9 @@ import (
 var _ = Describe("Anagram", func() {
 	DescribeTable("Anagram all",
 		func(word string, words []string, out []string) {
-			Expect(Anagrams(word,words)).To(Equal(out))
+			Expect(Anagrams(word, words)).To(Equal(out))
 		},
-		Entry("Empty", "wow", []string{}, []string{}),
+		Entry("Empty", "wow", []string{}, nil),
+		Entry("All are anagrams", "wow", []string{"oww", "wwo"}, []string{"oww", "wwo"}),
 	)
 })
