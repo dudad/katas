409dc7441f62466f1939500938eb974624ada5e9
dudad <damian.dudzinski@gmail.com>
2022-01-02 16:22:45 +0100

    commondenominators: full solution

--- Go/commondenominators/commondenominators.go
+++ Go/commondenominators/commondenominators.go
@@ -1,6 +1,42 @@
 package commondenominators
 
+import (
+	"bytes"
+	"fmt"
+)
+
+// GCD - Greatest common divisor
+func GCD(a int, b int) int {
+	for b != 0 {
+		tmp := b
+		b = a % b
+		a = tmp
+	}
+	return a
+}
+
+// LCM - Least common multiple
+func LCM(a int, b int) int {
+	return (a / GCD(a, b)) * b
+}
 
 func ConvertFracts(a [][]int) string {
-    return ""
+	if len(a) == 0 {
+		return ""
+	}
+
+	lcm := 1
+	for _, pair := range a {
+		lcm = LCM(lcm, pair[1])
+	}
+	var buffer bytes.Buffer
+
+	for _, pair := range a {
+
+		N := pair[0] * (lcm / pair[1])
+		D := pair[1] * (lcm / pair[1])
+		buffer.WriteString(fmt.Sprintf("(%d,%d)", N, D))
+	}
+
+	return buffer.String()
 }
--- Go/commondenominators/commondenominators_test.go
+++ Go/commondenominators/commondenominators_test.go
@@ -13,6 +13,30 @@ var _ = Describe("Commondenominators", func() {
 			Expect(ConvertFracts(in)).To(Equal(out))
 		},
 		Entry("Empty", [][]int{}, ""),
+		Entry("single", [][]int{{1, 2}}, "(1,2)"),
+		Entry("multi", [][]int{{1, 2}, {1, 3}, {1, 4}}, "(6,12)(4,12)(3,12)"),
+	)
+})
 
+var _ = Describe("Commondenominators", func() {
+	DescribeTable("Commondenominators - GCD",
+		func(a int, b int, out int) {
+			Expect(GCD(a, b)).To(Equal(out))
+		},
+		Entry("no GCD", 5, 6, 1),
+		Entry("some GCD", 200, 300, 100),
+		Entry("some GCD reorderd", 300, 200, 100),
+	)
+})
+
+var _ = Describe("Commondenominators", func() {
+	DescribeTable("Commondenominators - LCM",
+		func(a int, b int, out int) {
+			Expect(LCM(a, b)).To(Equal(out))
+		},
+		Entry("one of numder is LCM", 6, 12, 12),
+		Entry("when no common GCD", 5, 6, 30),
+		Entry("with some >1 GCD", 200, 300, 600),
+		Entry("with some >1 GCD reoreder args", 300, 200, 600),
 	)
 })
